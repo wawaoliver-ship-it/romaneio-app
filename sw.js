1c44bd2b56f92695c37a3b48682c844b2b412204
wawaoliver-ship-it <wawa.oliver@gmail.com>
2025-08-25 19:01:28 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,3 @@
-self.addEventListener("install", e => {
-  e.waitUntil(caches.open("romaneio-v1").then(cache => cache.addAll(["/", "/index.html"])))
-});
-self.addEventListener("fetch", e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)))
-});
+const CACHE="romaneio-v1";
+self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./manifest.webmanifest"])))});
+self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
