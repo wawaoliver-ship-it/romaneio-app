fdfcb73255cd2b15e17daa57fb356ceced29166e
wawaoliver-ship-it <wawa.oliver@gmail.com>
2025-08-26 07:17:50 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,7 @@
-const CACHE="romaneio-v15";
-self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./manifest.webmanifest"])))});
-self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
+const CACHE = "romaneio-v15";
+self.addEventListener("install", e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./app.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"])));
+});
+self.addEventListener("fetch", e=>{
+  e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request)));
+});
