2143364d791f3a83281e9c370f633ee53affec76
wawaoliver-ship-it <wawa.oliver@gmail.com>
2025-08-28 09:15:38 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // troque a versão para forçar atualização quando editar o app
-const CACHE = "romaneio-v20";
+const CACHE = "romaneio-v21";
 
 self.addEventListener("install", (e) => {
   e.waitUntil(
